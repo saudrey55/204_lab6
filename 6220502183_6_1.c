9c3237e76e56061ed993cae08e006595dce3b734
saudrey <weerawutearth@gmail.com>
2021-03-18 14:34:39 +0700

    ป

---
+++ 6220502183_6_1.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+#include<string.h>
+
+int main()
+{
+    int a,i;
+
+    scanf("%d",&a);
+
+    char z[a];
+
+    for ( i = 0; i < a; i++)
+    {
+        scanf("%c",&z[i]);
+    }
+    
+    
+
+}
