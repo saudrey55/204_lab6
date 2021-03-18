549b87c766b188d39fc2ddf2f04be70c5bfbf65a
saudrey <weerawutearth@gmail.com>
2021-03-18 17:19:38 +0700

    c

---
+++ 6220502183_6_4.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+
+int main()
+{
+    int n,i,j;
+    
+    scanf("%d",&n);
+
+    int z[n][2];
+
+    for ( i = 0; i < n ; i++)
+    {
+        for ( j = 0; j < 2; j++)
+        {
+            scanf("%d",&z[i][j]);
+        }
+        
+    }
+    
+}
