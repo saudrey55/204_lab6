f0ca8b7c9f462dfd6b15641e47f5d1fe1a8de61b
saudrey <weerawutearth@gmail.com>
2021-03-18 16:35:55 +0700

    c

--- 6220502183_6_2.c
+++ 6220502183_6_2.c
@@ -7,13 +7,13 @@ int main()
     scanf("%d",&n);
 
     int a[n],b[n];
-	for( i = 0; i <= n-1; i++)
-	{
+	for(i=0;i<n;i++)
+    {
 		scanf("%d",&a[i]);
 	}
-    for( i = 0; i <=n-1; i++)
-	{
-		scanf("%d",&b[i]);
+    for(i=0;i<n;i++)
+    {
+		scanf("%d",&a[i]);
 	}
 
     for ( i = 0; i < n; i++)
--- 6220502183_6_6.c
+++ 6220502183_6_6.c
@@ -7,6 +7,76 @@ int main()
  scanf("%d",&n);
  scanf("%s",x);
  int a[n][n];
-
- 
+ for(i=0;i<n;i++)
+ {
+  for(j=0;j<n;j++)
+  {
+   a[i][j]='.';
+  }
+ }
+ int b=0,c=0;
+ for(i=0;i<strlen(x);i++)
+ {
+  if(x[i]=='U')
+  {
+   if(a[b][c]=='.'||a[b][c]=='|')
+   a[b][c]='|';
+   else if(a[b][c]!='-'||a[b][c]!='+')
+   a[b][c]='+';
+   if(b-1>=0)
+   b--;
+   if(a[b][c]=='.'||a[b][c]=='|')
+   a[b][c]='|';
+   else if(a[b][c]!='-'||a[b][c]!='+')
+   a[b][c]='+';
+  }
+  else if(x[i]=='D')
+  {
+   if(a[b][c]=='.'||a[b][c]=='|')
+   a[b][c]='|';
+   else if(a[b][c]!='-'||a[b][c]!='+')
+   a[b][c]='+';
+   if(b+1<n)
+   b++;
+   if(a[b][c]=='.'||a[b][c]=='|')
+   a[b][c]='|';
+   else if(a[b][c]!='-'||a[b][c]!='+')
+   a[b][c]='+';
+  }
+  else if(x[i]=='R')
+  {
+   if(a[b][c]=='.'||a[b][c]=='-')
+   a[b][c]='-';
+   else if(a[b][c]!='|'||a[b][c]!='+')
+   a[b][c]='+';
+   if(c+1<n)
+   c++;
+   if(a[b][c]=='.'||a[b][c]=='-')
+   a[b][c]='-';
+   else if(a[b][c]!='|'||a[b][c]!='+')
+   a[b][c]='+';
+  }
+  else if(x[i]=='L')
+  {
+   if(a[b][c]=='.'||a[b][c]=='-')
+   a[b][c]='-';
+   else if(a[b][c]!='|'||a[b][c]!='+')
+   a[b][c]='+';
+   if(c-1>=0)
+   c--;
+   if(a[b][c]=='.'||a[b][c]=='-')
+   a[b][c]='-';
+   else if(a[b][c]!='|'||a[b][c]!='+')
+   a[b][c]='+';
+  }
+ }
+ for(i=0;i<n;i++)
+ {
+  for(j=0;j<n;j++)
+  {
+   printf("%c",a[i][j]);
+  }
+  printf("\n");
+ }
+ return 0;
 }
